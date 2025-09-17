dd21522677ae27a5bcd1ae07050a07b92cd1830e
stewardllc <stewardllc@gmail.com>
2025-09-17 15:14:34 +0000

    feat: configure next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
 };
 
 export default nextConfig;
